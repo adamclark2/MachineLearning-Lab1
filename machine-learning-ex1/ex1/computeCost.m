790a96b2e194a565992abd9098aadcff900afb97
Adam <adam.clark2@maine.edu>
2018-09-06 17:41:20 -0400

    Added compute cost (See Comment & Code)

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -19,4 +19,20 @@ J = 0;
 
 % =========================================================================
 
+# J(theta) := (1/2m) (SUM(   (h(x(i)) - y(i))^2   ) from i to m)
+# h is a line (y=mx+b) in the form h = theta[0] + theta[1]x
+# h is also theta^T x
+
+    tmpSum = 0;
+    m = length(X);
+    for i = 1:m
+        # As input, theta is a row vector and X is a column vector.
+        # We would like theta to be a column vector and X to be a row vector
+        # So each is transposed...
+        h = theta' * X(i, :)';
+
+        tmpSum = tmpSum + (h - y(i))^2;
+    endfor
+
+    J = (1/(2*m)) * tmpSum;
 end
